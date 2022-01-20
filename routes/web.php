0fe8afb89920db509cac2bc50e605ff5ad6c4619
Fafurin <ffafurin@mail.ru>
2022-01-20 12:15:50 +0300

    hw2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', [\App\Http\Controllers\WelcomeController::class, 'index']);
+
+Route::get('/auth', [\App\Http\Controllers\AuthorizationController::class, 'index']);
+
+Route::get('/categories', [\App\Http\Controllers\CategoryController::class, 'index']);
+Route::get('/categories/{id}', [\App\Http\Controllers\CategoryController::class, 'category'])
+        ->where('id', '[0-9]+');
+
+Route::resource('/admin/news', \App\Http\Controllers\Admin\NewsController::class);
+
